6d1aff901d407d0e9c252d9325366dce18cdd535
rockleon <ameyphantom07@gmail.com>
2021-04-28 16:15:35 +0530

    added session-storage check

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-module.exports = function updateVisits(key) {
+module.exports = function siteVisits(key, sessionStorage) {
   if (typeof key !== "string") throw new TypeError("Key should be a string!");
-  console.log('count updated');
+  const isVisited = sessionStorage.getItem('site-visited')
+  if(isVisited && isVisited != '') {
+    console.log('count not updated', isVisited);
+  }
+  else {
+    console.log('count updated', key, isVisited);
+    sessionStorage.setItem('site-visited', 'true')
+  }
+    // const url = 'https://bookit-v1.herokuapp.com/api/v1/bookings';
+  // fetch(url)
+  // .then(response => response.json())
+  // .then(data => console.log(data));
   return 1;
 };
+
+// class siteVisit {
+//   constructor(key) {
+//     this.key = key;
+//     this.updated = false
+//     console.log('count updated', key);
+//     this.updated = true
+//   }
+// }
+
+// module.exports = { siteVisit };
