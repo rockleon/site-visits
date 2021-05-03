6941d6be1b12c647b86f2cff3d89496576658362
rockleon <ameyphantom07@gmail.com>
2021-05-03 20:42:32 +0530

    api call and version change

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 module.exports = function siteVisits(key, sessionStorage) {
+  const url = "https://site-visits.herokuapp.com/visits";
   if (typeof key !== "string") throw new TypeError("Key should be a string!");
-  const isVisited = sessionStorage.getItem('site-visited')
-  if(isVisited && isVisited != '') {
-    console.log('count not updated', isVisited);
+  const isVisited = sessionStorage.getItem("site-visited");
+  if (isVisited && isVisited != "") {
+    console.log("count not updated", isVisited);
+  } else {
+    fetch(`${url}/${key}/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        sessionStorage.setItem("site-visited", "true");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
-  else {
-    console.log('count updated', key, isVisited);
-    sessionStorage.setItem('site-visited', 'true')
-  }
-    // const url = 'https://bookit-v1.herokuapp.com/api/v1/bookings';
-  // fetch(url)
-  // .then(response => response.json())
-  // .then(data => console.log(data));
   return 1;
 };
-
-// class siteVisit {
-//   constructor(key) {
-//     this.key = key;
-//     this.updated = false
-//     console.log('count updated', key);
-//     this.updated = true
-//   }
-// }
-
-// module.exports = { siteVisit };
